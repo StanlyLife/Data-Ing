0e31433d5958a7daf56a1a69840e7195fe97305f
Stian have <stianhave@hotmail.com>
2020-10-23 14:54:32 +0200

    Added inputhandlers and other assignments

--- Kodesonen-hjelp/Timber/main.js
+++ Kodesonen-hjelp/Timber/main.js
@@ -1,29 +1,51 @@
+function SolveEuler17() {
+	return AllNumberNames(1000);
+}
+
+function AllNumberNames(N) {
+	let numberString = "";
+	for (let i = 1; i <= N; i++) {
+		numberString += NumberName(i) + ", ";
+	}
+	let modifiedNumberString = numberString.replace(/-/g, "");
+	modifiedNumberString = modifiedNumberString.replace(/,/g, "");
+	modifiedNumberString = modifiedNumberString.replace(/ /g, "");
+	modifiedNumberString.trim();
+	return modifiedNumberString.length;
+}
+
 function NumberName(N) {
 	let digits = [];
+	let numberString = "";
 	digits = SplitNumber(N);
-	if (N <= 12) {
-		return SingleDigits(N);
+	if (N <= 19) {
+		numberString = SingleDigits(N);
 	} else if (N < 100) {
-		return DoubleDigits(digits[0]) + " " + SingleDigits(digits[1]);
+		numberString = DoubleDigits(digits[0]) + " ";
+		if (digits[1] > 0) {
+			numberString += SingleDigits(digits[1]);
+		}
 	} else if (N < 1000) {
-		if (TwoDigitsToNumber(digits[1], digits[2]) > 12) {
-			return (
-				TrippleDigits(digits[0]) +
-				" and " +
-				DoubleDigits(digits[1]) +
-				" " +
-				SingleDigits(digits[2])
-			);
-		} else {
-			return (
-				TrippleDigits(digits[0]) +
-				" and " +
-				SingleDigits(TwoDigitsToNumber(digits[1], digits[2]))
-			);
+		numberString += TrippleDigits(digits[0]);
+		if (TwoDigitsToNumber(digits[1], digits[2]) > 0) {
+			numberString += " and ";
+			if (TwoDigitsToNumber(digits[1], digits[2]) > 19) {
+				numberString += DoubleDigits(digits[1]);
+				if (digits[2] > 0) {
+					numberString += " ";
+					numberString += SingleDigits(digits[2]);
+				}
+			} else {
+				numberString += SingleDigits(
+					TwoDigitsToNumber(digits[1], digits[2])
+				);
+			}
 		}
 	} else if (N == 1000) {
 		return "thousand";
 	}
+
+	return numberString;
 }
 
 function TwoDigitsToNumber(one, two) {
@@ -68,6 +90,20 @@ function SingleDigits(N) {
 			return "eleven";
 		case 12:
 			return "twelve";
+		case 13:
+			return "thirteen";
+		case 14:
+			return "fourteen";
+		case 15:
+			return "fifteen";
+		case 16:
+			return "sixteen";
+		case 17:
+			return "seventeen";
+		case 18:
+			return "eighteen";
+		case 19:
+			return "nineteen";
 		default:
 			return "error";
 	}
@@ -98,4 +134,20 @@ function TrippleDigits(N) {
 	return SingleDigits(N) + "-hundred";
 }
 
-console.log(NumberName(112));
+console.log(NumberName(1));
+console.log(AllNumberNames(5));
+console.log(SolveEuler17());
+
+//Below this line javascript code is irrelevant to the assignment
+//The code is only used to enhance the website
+
+const numberInput = document.querySelector(".numberInput");
+const numberName = document.querySelector(".NumberName");
+const allNumberNames = document.querySelector(".AllNumberNames");
+const solveEuler = document.querySelector(".SolveEuler17");
+
+numberInput.addEventListener("input", (e) => {
+	numberName.value = NumberName(parseInt(numberInput.value));
+	allNumberNames.value = AllNumberNames(parseInt(numberInput.value));
+	solveEuler.value = SolveEuler17();
+});
